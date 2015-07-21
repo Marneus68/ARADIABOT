1934becb5994147a48631d5a55528b68f4963d0f
Duane Bekaert <duane.bekaert@gmail.com>
2015-07-21 12:10:59 +0000

    Basic ARADIABOT now working with ping request working.

--- ARADIABOT.cpp
+++ ARADIABOT.cpp
@@ -5,8 +5,11 @@
 #include <unistd.h>
 #include <netdb.h>
 
+#include <iterator>
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
 
 #define MAXLINE 4096
 
@@ -23,23 +26,27 @@ int _read(int sock, char* buf) {
                 *(buf+1)='\0';
                 return len;
             }
+    return 0;
 }
 
 int main(int argc, const char *argv[])
 {
     unsigned int port = 6667;
-    char *server_hostname = "irc.rizon.net";
+    char *server_hostname = (char*)"irc.rizon.net";
     char server[] = "";
     int sockfd, i;
 
     struct hostent *he;
     struct in_addr **addr_list;
          
+    std::cout << "Getting IP list for the hostname " << server_hostname << 
+        "..." << std::endl;
     if ((he = gethostbyname(server_hostname)) == NULL)
         return 1;
  
     addr_list = (struct in_addr **) he->h_addr_list;
      
+    std::cout << "Selecting IP address..." << std::endl;
     for(i = 0; addr_list[i] != NULL; i++)  {
         strcpy(server , inet_ntoa(*addr_list[i]));
         break;
@@ -66,13 +73,27 @@ int main(int argc, const char *argv[])
     std::string userstring = "USER ARADIABOT " + 
             std::string(server_hostname) + " bla :Aradia Megido\r\n";
     _send(sockfd, userstring);
+    std::cout << "Joining channel..." << std::endl;
     _send(sockfd, "JOIN #iridia\r\n");
 
     char recvline[MAXLINE];
     while(_read(sockfd, recvline)) {
-        std::string line = recvline;
-        std::cout << line;
-    }
 
+        std::string s = recvline;
+        std::stringstream ss(s);
+        std::istream_iterator<std::string> begin(ss);
+        std::istream_iterator<std::string> end;
+        std::vector<std::string> vstrings(begin, end);
+
+        std::string::size_type loc = s.find( "PING", 0 );
+        if(loc != std::string::npos ){
+            if (vstrings[2].compare("ARADIABOT")==0) {
+                std::cout << "Ping request received." << std::endl;
+                std::string pong = "PONG :" + vstrings[4] + "\r\n";
+                _send(sockfd, pong);
+                _send(sockfd, "PRIVMSG #iridia :ribbit\r\n");
+            }
+        }
+    }
     return 0;
 }
